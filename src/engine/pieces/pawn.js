bdf9a7059d9e394834813c36eba849f628692b0a
Charlie Quin <charliequin@outlook.com>
2019-05-21 15:39:05 +0100

    Passes tests for pawn movement up/down

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Player from  '../player'
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +7,12 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+
+        (this.player === Player.WHITE) ? currentSquare.row++ : currentSquare.row--;
+
+        return [currentSquare];
     }
 }
+
+
